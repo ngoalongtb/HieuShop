50e57c97ca2a44561d9defc8449611aa8ea3664c
DESKTOP-C0L1ALS\ngoal <ngoalongtb001@gmail.com>
2018-05-02 22:29:00 +0700

    delevery 1

--- EFCore/Service/SlideService.cs
+++ EFCore/Service/SlideService.cs
@@ -16,7 +16,7 @@ namespace EFCore.Service
             db = new ShopDB();
         }
         public List<Slide> ListAll()
-        {
+        { 
             return db.Slide.Where(x => x.Status == true).OrderBy(y => y.DisplayOrder).ToList();
         }
         public IEnumerable<Slide> listAll(string searchString, int page, int pageSize)
